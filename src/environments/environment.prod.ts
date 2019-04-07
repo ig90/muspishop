6df935765e76ba41705ad288238eaef1cfd9fc23
Igor <igor.filipiuk@gmail.com>
2019-04-08 00:52:16 +0200

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB6gpd5P7rhS1EoAbohE2rgwaSX_Go_OLo",
+    authDomain: "muspishop.firebaseapp.com",
+    databaseURL: "https://muspishop.firebaseio.com",
+    projectId: "muspishop",
+    storageBucket: "muspishop.appspot.com",
+    messagingSenderId: "932347279317"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB6gpd5P7rhS1EoAbohE2rgwaSX_Go_OLo",
+    authDomain: "muspishop.firebaseapp.com",
+    databaseURL: "https://muspishop.firebaseio.com",
+    projectId: "muspishop",
+    storageBucket: "muspishop.appspot.com",
+    messagingSenderId: "932347279317"
+  }
 };
 
 /*
