a9f04259140a7fa00f89e1fab393729c3900e71d
Igor <igor.filipiuk@gmail.com>
2019-04-09 01:09:48 +0200

    added bs-navbar component

---
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-navbar',
+  templateUrl: './bs-navbar.component.html',
+  styleUrls: ['./bs-navbar.component.sass']
+})
+export class BsNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
